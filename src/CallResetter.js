b03ed418917966bcd0fe1232430c24b42520df3d
Sunny <embodiedsunshine@gmail.com>
2023-05-23 07:14:21 -0600

    refactored callResetter into its own file

---
+++ src/CallResetter.js
@@ -0,0 +1,28 @@
+export default class CallResetter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(event, handler) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(handler);
+  }
+
+  off(event, handler) {
+    if (!this.events[event]) {
+      return;
+    }
+    const indexToRemove = this.events[event].indexOf(handler);
+    this.events.splice(indexToRemove, 1);
+  }
+
+  emit(event, data) {
+    if (!this.events[event]) {
+      return;
+    }
+    const handlers = this.events[event];
+    for (const handler of handlers) { handler(data) }
+  }
+}
