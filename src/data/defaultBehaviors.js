3344a25a60520af24bddc237ce9d084bbb6120b9
Sunny <embodiedsunshine@gmail.com>
2023-06-08 08:35:27 -0600

    created data folder to hold defaults for program values

---
+++ src/data/defaultBehaviors.js
@@ -0,0 +1,7 @@
+export const defaultBehaviors = "Acknowledge\nProbing Questions\nWhat Else?\nAccount Audit\nNBA\nRecap";
+export const defaultBehaviorsArray = defaultBehaviors.split("\n");
+
+export default {
+    string: defaultBehaviors,
+     array: defaultBehaviorsArray,
+}
---
+++ src/data/defaultConfig.js
@@ -0,0 +1,8 @@
+// default Configuration contains sensible values
+const defaultConfig = {
+  autostartTimer: false, 
+  alertInterval: 5,
+  editBehavior: false,
+}
+
+export default defaultConfig;
